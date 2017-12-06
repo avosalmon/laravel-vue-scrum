e3d089e01bf84164c3b3c80f6afe20df2701a827
Ryuta Hamasaki <avosalmon@gmail.com>
2017-12-06 22:58:50 +0900

    feat: implement controller util methods

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,38 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    /**
+     * Make response meta array.
+     *
+     * @param  \Illuminate\Database\Eloquent\Model|Collection|static $model
+     * @param  int $total
+     * @return array
+     */
+    protected function generateResponseMeta($model, $total = 0)
+    {
+        return [
+            'offset'    => $model->getOffset(),
+            'limit'     => $model->getLimit(),
+            'sort'      => $model->getSort(),
+            'direction' => $model->getDirection(),
+            'total'     => $total
+        ];
+    }
+
+    /**
+     * Make response array out of input.
+     *
+     * @param  string $type
+     * @param  \Illuminate\Database\Eloquent\Model|Collection|static $data
+     * @param  array $meta
+     * @return array
+     */
+    protected function formatResponse($type, $data = null, $meta = null)
+    {
+        return [
+            $type  => $data,
+            'meta' => $meta
+        ];
+    }
 }
