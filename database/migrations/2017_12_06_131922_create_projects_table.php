90fb2ec53e06e7916e678b4e4bb8cadc47fc1804
Ryuta Hamasaki <avosalmon@gmail.com>
2017-12-08 00:43:40 +0900

    feat: add active column to projects table

--- database/migrations/2017_12_06_131922_create_projects_table.php
+++ database/migrations/2017_12_06_131922_create_projects_table.php
@@ -16,6 +16,7 @@ class CreateProjectsTable extends Migration
         Schema::create('projects', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
+            $table->boolean('active')->default(1);
             $table->timestamps();
         });
     }
