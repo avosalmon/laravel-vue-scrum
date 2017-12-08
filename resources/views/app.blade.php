11e73eb8ced1debb075ecce004e41669f784cb13
Ryuta Hamasaki <avosalmon@gmail.com>
2017-12-08 20:54:10 +0900

    feat: add web route that returns app template

---
+++ resources/views/app.blade.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Laravel Vue Scrum</title>
+        <link rel="stylesheet" href="css/app.css">
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+    </head>
+    <body>
+        <div id="app">
+            <example-component></example-component>
+        </div>
+        <script src="js/app.js"></script>
+    </body>
+</html>
