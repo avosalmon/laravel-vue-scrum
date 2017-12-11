929ca44526ad358bbc7895cfe7dedc4ebdedbb11
Ryuta Hamasaki <avosalmon@gmail.com>
2017-12-12 07:57:48 +0900

    build(npm): install vue-material

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -10,6 +10,13 @@ require('./bootstrap');
 window.Vue = require('vue');
 
 /**
+ * Import Vue Material
+ */
+import VueMaterial from 'vue-material'
+Vue.use(VueMaterial)
+import 'vue-material/dist/vue-material.css'
+
+/**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
  * or customize the JavaScript scaffolding to fit your unique needs.
