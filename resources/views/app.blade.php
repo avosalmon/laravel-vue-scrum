3d5184fd6a8cee6cfcdc64bffeb9d9deaf68c18c
Ryuta Hamasaki <avosalmon@gmail.com>
2017-12-13 23:55:45 +0900

    feat: rename app title

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,7 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Laravel Vue Scrum</title>
+  <title>Scrum Velocity Manager</title>
   <link rel="stylesheet" href="css/app.css">
 </head>
 <body>
