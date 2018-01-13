8964b326ac3ef449908dbcda565ac5ce6a3db993
Ryuta Hamasaki <avosalmon@gmail.com>
2018-01-13 17:13:21 +0900

    refactor(mutation): remove unnecessary mutations

--- resources/assets/js/modules/sprints/_store/mutations.js
+++ resources/assets/js/modules/sprints/_store/mutations.js
@@ -2,18 +2,6 @@ const GET_SPRINTS_SUCCESS = (state, response) => {
   state.sprints = response.data.sprints
 }
 
-const CREATE_SPRINT_SUCCESS = (state, response) => {
-
-}
-
-const UPDATE_SPRINT_SUCCESS = (state, response) => {
-
-}
-
-const DESTROY_SPRINT_SUCCESS = (state, response) => {
-
-}
-
 const GET_USERS_SUCCESS = (state, response) => {
   state.users = response.data.users
 }
@@ -24,9 +12,6 @@ const GET_PROJECTS_SUCCESS = (state, response) => {
 
 export default {
   GET_SPRINTS_SUCCESS,
-  CREATE_SPRINT_SUCCESS,
-  UPDATE_SPRINT_SUCCESS,
-  DESTROY_SPRINT_SUCCESS,
   GET_USERS_SUCCESS,
   GET_PROJECTS_SUCCESS
 }
