053eaf21bda13b1da6e52629b9fae84b149983c7
Ryuta Hamasaki <avosalmon@gmail.com>
2018-01-28 21:18:47 +0900

    build: enable asset versioning

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,10 +5,10 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Scrum Velocity Manager</title>
-  <link rel="stylesheet" href="css/app.css">
+  <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
 </head>
 <body>
   <div id="app"></div>
-  <script src="js/app.js"></script>
+  <script src="{{ mix('js/app.js') }}"></script>
 </body>
 </html>
